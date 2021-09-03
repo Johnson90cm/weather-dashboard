1b3f10b211545ccce6ba89d8192f1b2112ef8fde
Tank <johnson90.cm@gmail.com>
2021-09-03 10:47:23 -0600

    humidity and uv index

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,18 +8,29 @@ var searchButtonEl = document.querySelector("#search-button")
 var cityTerm = document.getElementById('city-search').value;
 
 
-fetch ("http://api.weatherapi.com/v1/forecast.json?key=0d8aceedf9ac4705837152922210309&q=paris&days=5&aqi=no&alerts=no")
+fetch("http://api.weatherapi.com/v1/forecast.json?key=0d8aceedf9ac4705837152922210309&q=paris&days=5&aqi=no&alerts=no")
 
-.then(function(cityResponse) {
-    return cityResponse.json();
-})
+    .then(function (cityResponse) {
+        return cityResponse.json();
+    })
 
-.then(function(cityResponse) {
-    console.log(cityResponse)
-    citySearchTerm.innerHTML = cityResponse.location.name
-    var currentCityTemp = document.querySelector("#current-city-temp")
-    currentCityTemp.innerHTML = cityResponse.current.temp_f
-})
+    .then(function (cityResponse) {
+        console.log(cityResponse)
+        // return searched name
+        citySearchTerm.innerHTML = cityResponse.location.name
+        // return searched city temp
+        var currentCityTemp = document.querySelector("#current-city-temp")
+        currentCityTemp.innerHTML = cityResponse.current.temp_f
+        // return searched city wind
+        var currentCityTemp = document.querySelector("#current-city-wind")
+        currentCityTemp.innerHTML = cityResponse.current.wind_mph
+        // return searched city Humidity
+        var currentCityTemp = document.querySelector("#current-city-humidity")
+        currentCityTemp.innerHTML = cityResponse.current.humidity
+        // return searched city UV Index
+        var currentCityTemp = document.querySelector("#current-city-uv-index")
+        currentCityTemp.innerHTML = cityResponse.current.uv
+    })
 
 
 var searchSubmitHandler = function (event) {
