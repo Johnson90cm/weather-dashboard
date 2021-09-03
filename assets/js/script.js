168b8ad658097b7bfbbcde5eea31dc343f0e4583
Tank <johnson90.cm@gmail.com>
2021-09-03 12:33:22 -0600

    added weather all weather data, time, and bugged icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,10 @@ var cityContainerEl = document.querySelector("#city-container")
 var citySearchTerm = document.querySelector("#current-city")
 var cityButtonsEl = document.querySelector("#city-buttons")
 var searchButtonEl = document.querySelector("#search-button")
-
 var cityTerm = document.getElementById('city-search').value;
 
-
-fetch("http://api.weatherapi.com/v1/forecast.json?key=0d8aceedf9ac4705837152922210309&q=paris&days=5&aqi=no&alerts=no")
+// fetch data from weatherAPI.com
+fetch("http://api.weatherapi.com/v1/forecast.json?key=0d8aceedf9ac4705837152922210309&q=paris&days=7&aqi=no&alerts=no")
 
     .then(function (cityResponse) {
         return cityResponse.json();
@@ -16,8 +15,12 @@ fetch("http://api.weatherapi.com/v1/forecast.json?key=0d8aceedf9ac47058371529222
 
     .then(function (cityResponse) {
         console.log(cityResponse)
-        // return searched name
+        // return searched name, date & icon
+        var currentDate = document.querySelector("#current-date")
+        var currentDateIcon = document.querySelector("#current-date-weather-icon")
         citySearchTerm.innerHTML = cityResponse.location.name
+        currentDate.innerHTML = cityResponse.location.localtime
+        currentDateIcon.innerHTML = cityResponse.current.condition.icon
         // return searched city temp
         var currentCityTemp = document.querySelector("#current-city-temp")
         currentCityTemp.innerHTML = cityResponse.current.temp_f
@@ -31,7 +34,6 @@ fetch("http://api.weatherapi.com/v1/forecast.json?key=0d8aceedf9ac47058371529222
         var currentCityUvIndex = document.querySelector("#current-city-uv-index")
         currentCityUvIndex.innerHTML = cityResponse.current.uv
     })
-
 
 var searchSubmitHandler = function (event) {
     event.preventDefault();
