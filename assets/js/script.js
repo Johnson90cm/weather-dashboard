a2356a3c39ca7438e34a7d29194ff885b54c8f86
Tank <johnson90.cm@gmail.com>
2021-09-04 12:47:44 -0600

    working day 1 forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,12 +15,16 @@ fetch("http://api.weatherapi.com/v1/forecast.json?key=0d8aceedf9ac47058371529222
 
     .then(function (cityResponse) {
         console.log(cityResponse)
-        // return searched name, date & icon
-        var currentDate = document.querySelector("#current-date")
-        var currentDateIcon = document.querySelector("#current-date-weather-icon")
+        // return searched name
         citySearchTerm.innerHTML = cityResponse.location.name
+        // return searched date & time
+        var currentDate = document.querySelector("#current-date")
         currentDate.innerHTML = cityResponse.location.localtime
-        currentDateIcon.innerHTML = cityResponse.current.condition.icon
+        // return searched date weather icon
+        var currentDateIcon = document.querySelector("#current-date-weather-icon")
+        var iconImg = document.createElement('img');
+        iconImg.setAttribute('src', cityResponse.current.condition.icon)
+        currentDateIcon.appendChild(iconImg)
         // return searched city temp
         var currentCityTemp = document.querySelector("#current-city-temp")
         currentCityTemp.innerHTML = cityResponse.current.temp_f
@@ -33,6 +37,32 @@ fetch("http://api.weatherapi.com/v1/forecast.json?key=0d8aceedf9ac47058371529222
         // return searched city UV Index
         var currentCityUvIndex = document.querySelector("#current-city-uv-index")
         currentCityUvIndex.innerHTML = cityResponse.current.uv
+
+
+        // forecast bg
+        var forecastBackground = document.querySelector("#weekday-item")
+        forecastBackground.classList.add("bg-secondary", "text-white")
+
+        // forecast day 1
+
+        // date
+        var dayOneDate = document.querySelector("#weekday-item-1-date")
+        dayOneDate.innerHTML = cityResponse.forecast.forecastday[0].date
+        // icon
+        var currentDateIcon1 = document.querySelector("#weekday-item-1-icon")
+        var iconImg1 = document.createElement('img');
+        iconImg1.setAttribute('src', cityResponse.forecast.forecastday[0].day.condition.icon)
+        currentDateIcon1.appendChild(iconImg1)
+        // temp
+        var dayOneTemp = document.querySelector("#weekday-item-1-temp")
+        dayOneTemp.innerHTML = cityResponse.forecast.forecastday[0].day.avgtemp_f
+        // wind
+        var dayOneWind = document.querySelector("#weekday-item-1-wind")
+        dayOneWind.innerHTML = cityResponse.forecast.forecastday[0].day.maxwind_mph
+        // humidity
+        var dayOneHumidity = document.querySelector("#weekday-item-1-humidity")
+        dayOneHumidity.innerHTML = cityResponse.forecast.forecastday[0].day.avghumidity
+
     })
 
 var searchSubmitHandler = function (event) {
